387838764a3535233ec6bc6bc757c22a55c7a0cb
Nafie Sallahu <nafiesallahu1@gmail.com>
2022-08-13 13:09:20 +0200

    Delete fullstack directory

--- fullstack/server/routes/mobile.routes.js
+++
@@ -1,9 +0,0 @@
-const mobileController = require("../controllers/mobile.controller");
-
-module.exports = (app) => {
-  app.get("/api/mobiles", mobileController.getMobiles);
-  app.get("/api/mobiles/:id", mobileController.getMobileById);
-  app.post("/api/mobiles", mobileController.createMobile);
-  app.put("/api/mobiles/:id", mobileController.updateMobile);
-  app.delete("/api/mobiles/:id", mobileController.deleteMobile);
-};
