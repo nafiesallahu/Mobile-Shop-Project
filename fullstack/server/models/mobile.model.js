d72974e6cae5ea72a49c74dc7d2003e6f9454116
Nafie Sallahu <nafiesallahu1@gmail.com>
2022-08-15 22:55:46 +0200

    Updated Model

--- fullstack/server/models/mobile.model.js
+++ fullstack/server/models/mobile.model.js
@@ -4,23 +4,23 @@ const MobileSchema = mongoose.Schema(
   {
     title: {
       type: String,
-      required: [true, "mobile title is required"],
+      required: [true, "Mobile title is required"],
       minLength: [3, "Title must be at least 3 characters. "]
     },
     type: {
       type: String,
-      required: [true, "A mobile type is required!!"],
+      required: [true, "A mobile type is required"],
       enum: ["Iphone", "Samsung", "IPad", "Mac", "Accessories"]
     },
     boxArt: {
       type: String,
-      required: [true, "Mobile image is required"],
+      required: [true, "Mobile Image is required"],
       minLength: [10, "Image must be at least 10 characters. "]
     },
     price: {
       type: String,
       required: [true, "mobile price is required"],
-      minLength: [2, "Title must be at least 2 characters. "]
+      minLength: [2, "Price must be at least 2 characters. "]
     },
     details: {
       type: String
